03ef1d99679de121dd837f71a03fe97278a53f22
JJongmen <pjy90123@gmail.com>
2023-11-19 21:42:15 +0900

    chore: jjwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     testImplementation("org.springframework.security:spring-security-test")
 
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
