86ba377645b15787f205585ee7ba1d6e57e0c000
JJongmen <pjy90123@gmail.com>
2023-11-22 03:34:58 +0900

    refactor: UserTest에 @Nested 추가

--- src/test/kotlin/org/whatismytree/wimt/user/domain/UserTest.kt
+++ src/test/kotlin/org/whatismytree/wimt/user/domain/UserTest.kt
@@ -1,27 +1,31 @@
 package org.whatismytree.wimt.user.domain
 
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 import org.whatismytree.wimt.auth.domain.OAuthType
 import java.time.LocalDateTime
 
-class UserTest {
+internal class UserTest {
 
-    @Test
-    fun `softDelete시 deleteAt에 현재 시간이 설정된다`() {
-        // given
-        val email = "test@email.com"
-        val oauthType = OAuthType.KAKAO
-        val oauthId = "sampleOAuthId"
+    @Nested
+    inner class SoftDelete {
+        @Test
+        fun `softDelete시 deleteAt에 현재 시간이 설정된다`() {
+            // given
+            val email = "test@email.com"
+            val oauthType = OAuthType.KAKAO
+            val oauthId = "sampleOAuthId"
 
-        val user = User.of(email, oauthType, oauthId)
-        val now = LocalDateTime.now()
+            val user = User.of(email, oauthType, oauthId)
+            val now = LocalDateTime.now()
 
-        // when
-        user.softDelete()
+            // when
+            user.softDelete()
 
-        // then
-        assertThat(user.deletedAt).isAfterOrEqualTo(now)
+            // then
+            assertThat(user.deletedAt).isAfterOrEqualTo(now)
+        }
     }
 }
