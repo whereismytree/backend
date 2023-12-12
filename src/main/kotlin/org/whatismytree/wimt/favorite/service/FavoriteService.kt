e4d52befd43b84f428167f9994a3bc89bcb6d5ce
username1103 <mikim961103@gmail.com>
2023-12-12 23:54:00 +0900

    feat: 트리 저장/삭제 API 추가

---
+++ src/main/kotlin/org/whatismytree/wimt/favorite/service/FavoriteService.kt
@@ -0,0 +1,32 @@
+package org.whatismytree.wimt.favorite.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.whatismytree.wimt.favorite.domain.Favorite
+import org.whatismytree.wimt.favorite.repository.FavoriteRepository
+import org.whatismytree.wimt.tree.repository.TreeRepository
+
+@Service
+@Transactional(readOnly = true)
+class FavoriteService(
+    private val treeRepository: TreeRepository,
+    private val favoriteRepository: FavoriteRepository
+) {
+    @Transactional
+    fun updateFavoriteTree(userId: Long, treeId: Long, isFavorite: Boolean) {
+        treeRepository.findByIdAndDeletedAtIsNull(treeId)
+            ?: throw Exception("해당하는 트리가 존재하지 않습니다 treeId = $treeId")
+
+        val favorite = favoriteRepository.findByUserIdAndTreeId(userId, treeId)
+
+        if(isFavorite && favorite == null) {
+            favoriteRepository.save(Favorite.of(userId, treeId))
+            return
+        }
+
+        if(!isFavorite && favorite != null) {
+            favoriteRepository.delete(favorite)
+            return
+        }
+    }
+}
