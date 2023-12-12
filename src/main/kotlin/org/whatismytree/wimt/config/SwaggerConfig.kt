b7a7434410025cd92fdcf45de461c2d8d173a8e0
JJongmen <pjy90123@gmail.com>
2023-12-13 00:19:26 +0900

    feat: @CurrentUserId userId 파라미터가 스웨거에 안보이도록 설정

--- src/main/kotlin/org/whatismytree/wimt/config/SwaggerConfig.kt
+++ src/main/kotlin/org/whatismytree/wimt/config/SwaggerConfig.kt
@@ -3,8 +3,11 @@ package org.whatismytree.wimt.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import org.springdoc.core.customizers.OperationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.MethodParameter
+import org.whatismytree.wimt.common.CurrentUserId
 
 @Configuration
 class SwaggerConfig {
@@ -13,6 +16,25 @@ class SwaggerConfig {
     fun openAPI(): OpenAPI = OpenAPI()
         .components(Components())
         .info(apiInfo())
+
+    @Bean
+    fun customOperationCustomizer(): OperationCustomizer {
+        return OperationCustomizer { operation, handlerMethod ->
+            handlerMethod.methodParameters
+                .firstOrNull {
+                    it.parameterType == Long::class.java &&
+                        it.hasParameterAnnotation(CurrentUserId::class.java)
+                }
+                ?.let { paramToRemove ->
+                    operation.parameters.removeIf { param -> param.name == getParamName(paramToRemove) }
+                }
+            operation
+        }
+    }
+
+    private fun getParamName(methodParameter: MethodParameter): String =
+        methodParameter.parameterName
+            ?: methodParameter.parameter.declaringExecutable.parameters[methodParameter.parameterIndex].name
 
     private fun apiInfo() = Info()
         .title("어쩔트리 API")
