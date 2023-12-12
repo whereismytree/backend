2fb4592fe80a11c729babc70929b68dc9a7ee63f
JJongmen <pjy90123@gmail.com>
2023-12-13 02:02:10 +0900

    feat: @CurrentUserId에 회원 존재, 탈퇴 검증 추가

--- src/main/kotlin/org/whatismytree/wimt/config/WebConfig.kt
+++ src/main/kotlin/org/whatismytree/wimt/config/WebConfig.kt
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.whatismytree.wimt.auth.resolver.CurrentUserIdArgumentResolver
+import org.whatismytree.wimt.user.repository.UserRepository
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+    private val userRepository: UserRepository,
+) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
-        resolvers.add(CurrentUserIdArgumentResolver())
+        resolvers.add(CurrentUserIdArgumentResolver(userRepository))
     }
 }
