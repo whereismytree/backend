bbd5791cf0290bd65a2e90e6449391d27b70b344
username1103 <mikim961103@gmail.com>
2023-12-13 23:27:49 +0900

    feat: 내 리뷰 조회 API 추가

---
+++ src/main/kotlin/org/whatismytree/wimt/review/controller/dto/MyReviewResponse.kt
@@ -0,0 +1,39 @@
+package org.whatismytree.wimt.review.controller.dto
+
+import org.whatismytree.wimt.review.repository.dto.MyReviewResult
+import java.time.LocalDateTime
+
+data class MyReviewResponse(
+    val reviews: List<MyReviews>
+) {
+
+    val totalReviews: Int
+        get() = reviews.size
+
+    data class MyReviews(
+        val reviewId: Long,
+        val treeName: String,
+        val createdAt: LocalDateTime,
+        val reviewImageUrl: String,
+        val content: String,
+        val tags: List<String>
+    )
+
+
+    companion object {
+        fun of(reviews: List<MyReviewResult>): MyReviewResponse {
+            return MyReviewResponse(
+                reviews = reviews.map {
+                    MyReviews(
+                        reviewId = it.reviewId,
+                        treeName = it.treeName,
+                        createdAt = it.createdAt,
+                        content = it.content,
+                        reviewImageUrl = it.reviewImageUrl,
+                        tags = it.tags
+                    )
+                }
+            )
+        }
+    }
+}
