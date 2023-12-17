b590b7b218842c703a86de457209099656ec9007
kevin <jjyu9312@gmail.com>
2023-12-17 23:29:57 +0900

    @EnableScheduling 추가

--- src/main/kotlin/org/whatismytree/wimt/WimtApplication.kt
+++ src/main/kotlin/org/whatismytree/wimt/WimtApplication.kt
@@ -2,8 +2,10 @@ package org.whatismytree.wimt
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class WimtApplication
 
 fun main(args: Array<String>) {
