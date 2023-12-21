9ebee0f6e2fc6521f52923569fd064cefe6bba0c
kevin <79048655+jjyu9312@users.noreply.github.com>
2023-12-21 12:38:43 +0900

    Feature/planet scale db (#13)
    * 트리 관리 API controller 생성
    
    * 트리 관리 entity 생성
    
    * controller 위치 수정
    
    * entity 수정
    
    * test 전 1차 tree 조회 로직
    
    * test 전 1차 tree 조회 로직
    
    * tree crud
    
    * pr 수정
    
    * 컬럼 varchar로 변경
    
    * 트리 삭제 시 deletedAt 저장
    
    * tree 조회 api commit
    
    * pr comment 수정
    
    * response 명세서에 맞게 수정
    
    * deletedAt 체크 조건 추가
    
    * rollback
    
    * query 수정
    
    * psdb 연결
    
    * @EnableScheduling 추가

---
+++ src/main/kotlin/org/whatismytree/wimt/common/DatabaseConnectionScheduler.kt
@@ -0,0 +1,26 @@
+package org.whatismytree.wimt.common
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class DatabaseConnectionScheduler {
+
+    @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
+
+    @Scheduled(fixedRate = 6 * 60 * 60 * 1000)
+    fun executeDatabaseQuery() {
+        try {
+            val query = "select * from users"
+            val results = jdbcTemplate.queryForList(query)
+
+            println("Query executed successfully. Results: $results")
+        } catch (e: Exception) {
+
+            println("Error executing query: ${e.message}")
+        }
+    }
+}
