84ec141519add807a2cdc5ba5290bd2228c233f6
JJongmen <pjy90123@gmail.com>
2024-03-01 23:33:41 +0900

    fix: 지도에서 트리 조회가 삭제된 트리만 조회되던 버그 수정

--- src/main/kotlin/org/whatismytree/wimt/tree/repository/TreeRepository.kt
+++ src/main/kotlin/org/whatismytree/wimt/tree/repository/TreeRepository.kt
@@ -9,7 +9,7 @@ interface TreeRepository : JpaRepository<Tree, Long>, TreeRepositoryCustom {
 
     @Query(
         "SELECT t FROM Tree t " +
-            "WHERE (t.deletedAt IS NOT NULL)" +
+            "WHERE (t.deletedAt IS NULL)" +
             "AND (t.lat BETWEEN :bottomLeftLat AND :topLeftLat)" +
             "AND (t.lng BETWEEN :bottomLeftLng AND :bottomRightLng) " +
             "AND t.lat BETWEEN :bottomRightLat AND :topRightLat " +
